a0b291cc34c92c8172303016ae7e47ff2183a62b
Dmitro Pochapsky <pchpsky@outlook.com>
2021-02-24 01:21:11 +0200

    Setup tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :diary, Diary.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "admin",
   database: "diary_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
