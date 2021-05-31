cae6f733e46c0ff49021c5561dea97d3bc96aced
Dmitro Pochapsky <pchpsky@outlook.com>
2021-05-31 11:09:20 +0300

    Update header component

--- lib/diary_web/live/header_component.ex
+++ lib/diary_web/live/header_component.ex
@@ -2,6 +2,20 @@ defmodule DiaryWeb.HeaderComponent do
   use DiaryWeb, :live_component
   alias DiaryWeb.Gettext
 
+  def render(assigns) do
+    ~L"""
+    <header class="bg-th-<%= @page %> h-9 flex">
+      <div class="flex-1">
+      </div>
+      <div class="flex-none flex items-center justify-center">
+        <h1 class="font-semibold text-lg"><%= @title %></h1>
+      </div>
+      <div class="flex-1">
+      </div>
+    </header>
+    """
+  end
+
   def update(assigns, socket) do
     {:ok, assign(socket, [title: title(assigns.page), page: assigns.page])}
   end
