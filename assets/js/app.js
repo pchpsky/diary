95e52d7dc71c1b1edef158a3cf9c44366c94bdcd
Dmitro Pochapsky <pchpsky@outlook.com>
2021-06-05 17:12:42 +0300

    Animate form

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,13 +12,23 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
+import "alpinejs"
 import "phoenix_html"
 import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken },
+  dom: {
+    onBeforeElUpdated(from, to){
+      if(from.__x) {
+        window.Alpine.clone(from.__x, to)
+      }
+    }
+  }
+})
 
 // Show progress bar on live navigation and form submits
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
--- lib/diary_web/live/insulin_live/form_component.ex
+++ lib/diary_web/live/insulin_live/form_component.ex
@@ -1,10 +1,11 @@
 defmodule DiaryWeb.InsulinLive.FormComponent do
   use DiaryWeb, :live_component
 
-  def render(assigns) do
-    ~L"""
-    <div>
-    </div>
-    """
+  def update(_assigns, socket) do
+    {:ok, assign(socket, :expanded, false)}
+  end
+
+  def handle_event("toggle_form", _params, socket) do
+    {:noreply, assign(socket, :expanded, !socket.assigns.expanded)}
   end
 end
