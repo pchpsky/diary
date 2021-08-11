158994b01dcbee365a49ba5633afda933cc4c945
Dmitro Pochapsky <pchpsky@outlook.com>
2021-08-11 21:50:40 +0300

    Commit from web vs code

--- test/diary/accounts_test.exs
+++ test/diary/accounts_test.exs
@@ -37,7 +37,7 @@ defmodule Diary.AccountsTest do
   describe "get_user!/1" do
     test "raises if id is invalid" do
       assert_raise Ecto.NoResultsError, fn ->
-        Accounts.get_user!(-1)
+        Accounts.get_user!(-2)
       end
     end
 
