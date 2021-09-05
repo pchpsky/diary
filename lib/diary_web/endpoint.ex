bbb108d2dacdfd02afb1042372b4cd752d57f0d0
Dmytro Pochapskyi <pchpsky@outlook.com>
2021-09-05 20:33:15 +0300

    static images

--- lib/diary_web/endpoint.ex
+++ lib/diary_web/endpoint.ex
@@ -28,7 +28,7 @@ defmodule DiaryWeb.Endpoint do
     at: "/",
     from: :diary,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images icons favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
