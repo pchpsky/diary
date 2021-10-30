8dee174e5c995b6e9c2287fc7992026686fed024
Dmytro Pochapskyi <pchpsky@outlook.com>
2021-10-31 02:18:01 +0300

    Update deps

--- lib/diary_web/live/locales_live.ex
+++ lib/diary_web/live/locales_live.ex
@@ -1,7 +1,7 @@
 defmodule DiaryWeb.LocalesLive do
   import Phoenix.LiveView
 
-  def mount(_params, _session, socket) do
+  def on_mount(:default, _params, _session, socket) do
     case get_connect_params(socket) do
       %{"timezone" => tz} ->
         {:cont, assign(socket, timezone: tz)}
