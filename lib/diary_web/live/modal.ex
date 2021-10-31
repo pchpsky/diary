a352472d726f063150fd0b2d4a0a0e2696e8e49c
Dmytro Pochapskyi <pchpsky@outlook.com>
2021-10-31 12:55:42 +0200

    Update modal

--- lib/diary_web/live/modal.ex
+++ lib/diary_web/live/modal.ex
@@ -5,15 +5,22 @@ defmodule DiaryWeb.Modal do
     socket
     |> assign_new(:header, fn -> [] end)
     |> assign_new(:footer, fn -> [] end)
-    |> assign_new(:state, fn -> :open end)
+    |> assign_new(:state, fn -> :closed end)
     |> Result.ok()
   end
 
   def handle_event("close", _, socket) do
-    {:noreply, assign(socket, :state, :close)}
+    {:noreply, assign(socket, :state, :closed)}
   end
 
   def handle_event("open", _, socket) do
     {:noreply, assign(socket, :state, :open)}
+  end
+
+  defp targets(id, target) do
+    [id, target]
+    |> Enum.filter(& &1)
+    |> Enum.map(&"##{&1}")
+    |> Enum.join(",")
   end
 end
