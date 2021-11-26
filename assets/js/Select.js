8341e5d0cdf11c8dfb4fd263fc5bcca9f5dedf98
Dmytro Pochapskyi <pchpsky@outlook.com>
2021-11-27 00:41:40 +0200

    prevent select firefox

--- assets/js/Select.js
+++ assets/js/Select.js
@@ -1,7 +1,10 @@
 export default {
   mounted() {
     this.el.addEventListener('mousedown', e => {
+      e.stopPropagation();
       e.preventDefault();
+      this.el.blur();
+      window.focus();
     })
 
     this.el.addEventListener('selected', e => {
