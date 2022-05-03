96930b843599f79eb6ac4be3ab7689b9b760da2f
Dmytro Pochapskyi <pchpsky@outlook.com>
2022-05-03 14:42:31 +0300

    latest glucose

--- lib/diary_web/live/home_live.ex
+++ lib/diary_web/live/home_live.ex
@@ -7,11 +7,12 @@ defmodule DiaryWeb.HomeLive do
   @impl true
   def mount(_parms, _session, socket) do
     user = socket.assigns.current_user
+    glucose_units = Diary.Settings.get_glucose_units(user.id)
 
     latest_insulin = Metrics.latest_insulin(user.id) |> Diary.Repo.preload(:insulin)
     latest_glucose = Metrics.latest_glucose(user.id)
 
-    {:ok, assign(socket, latest_insulin: latest_insulin, latest_glucose: latest_glucose)}
+    {:ok, assign(socket, latest_insulin: latest_insulin, latest_glucose: latest_glucose, glucose_units: glucose_units)}
   end
 
   defp format_time(time) do
@@ -22,4 +23,7 @@ defmodule DiaryWeb.HomeLive do
   def handle_event("redirect", %{"to" => to}, socket) do
     {:noreply, push_redirect(socket, to: to)}
   end
+
+  defp format_units(:mmol_per_l), do: "mmol/L"
+  defp format_units(:mg_per_dl), do: "mg/dL"
 end
